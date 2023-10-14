81c92344a2d8818f1f5a64be0b9e78ad6f0c677d
Mateus Yamada Muller <mateusyamada@alunos.utfpr.edu.br>
2023-10-14 01:43:59 -0300

    Updated IO model (leontief) calculations

--- framework/file_exiobase3.py
+++ framework/file_exiobase3.py
@@ -21,12 +21,16 @@ class EXIOfiles:
         self.A = pd.DataFrame()
         self.Y = pd.DataFrame()
         self.M = pd.DataFrame()
+        self.S = pd.DataFrame()
+        self.F = pd.DataFrame()
+        self.Dcba = pd.DataFrame()
+        self.Z = pd.DataFrame()
 
     def read(self):
         print(f'[Class {self.__class__.__name__}] Reading {self.file}...')
 
         files = list()
-        names = ['A', 'Y', 'M']
+        names = ['A', 'Dcba','F', 'M', 'S', 'Y', 'Z']
         for nm in range(len(names)):
             files.append(names[nm] + str(self.year) + '.pkl')
         files.sort()
@@ -37,25 +41,33 @@ class EXIOfiles:
                 with open(self.path + 'pkls/' + file, 'rb') as curr_pkl:
                     pklIO.append(pickle.load(curr_pkl))
 
+            self.Z = pklIO.pop()
             self.Y = pklIO.pop()
+            self.S = pklIO.pop()
             self.M = pklIO.pop()
+            self.F = pklIO.pop()
+            self.Dcba = pklIO.pop()
             self.A = pklIO.pop()
 
         else:
             self.exio_raw = pymrio.parse_exiobase3(path=self.path + self.file)
 
             self.A = self.exio_raw.A
+            self.Z = self.exio_raw.Z
             self.Y = self.exio_raw.Y
             self.M = self.exio_raw.satellite.M
+            self.S = self.exio_raw.satellite.S
+            self.F = self.exio_raw.satellite.F
+            self.Dcba = self.exio_raw.satellite.D_cba
 
-            to_pkl = [self.A, self.Y, self.M]
+            to_pkl = [self.A, self.Dcba, self.F, self.M, self.S, self.Y, self.Z]
             for par in range(len(to_pkl)):
                 to_pkl[par].to_pickle(self.path + 'pkls/' + f'{names[par] + str(self.year)}.pkl')
 
         self.regFilter()
         print(f'[Class {self.__class__.__name__}] {self.file} read!')
 
-        return self.M, self.Y, self.A
+        return self.A, self.Dcba, self.F, self.M, self.S, self.Y, self.Z
 
     def regFilter(self):
         if self.region_filter:
@@ -69,23 +81,15 @@ class EXIOfiles:
                 if (self.Y.columns[clm])[0] in self.ISO_code:
                     clm_idx.append(clm)
 
+            strs_idx = list()
+            for strs in range(len(self.S.columns)):
+                if (self.S.columns[strs])[0] in self.ISO_code:
+                    strs_idx.append(strs)
+
             self.A = self.A.iloc[line_idx, line_idx]
+            self.Z = self.Z.iloc[line_idx, line_idx]
             self.Y = self.Y.iloc[line_idx, clm_idx]
             self.M = self.M.iloc[:,line_idx]
-
-    def StructAlgebra(self):
-        final_demand = (self.Y @ np.ones((np.shape(self.Y)[1], 1)))
-        my_x = np.linalg.inv(np.eye(len(self.A)) - self.A) @ final_demand
-        #ouput in M.EUR for the given demand Y
-        print(f'[Output matrix x] MSE = {mean_squared_error(self.x.values, my_x.values)}')
-
-        my_M = self.exio_raw.satellite.S @ np.linalg.inv(np.eye(len(self.A)) - self.A)
-        #direct and indirect requirements for the respective unit of column output
-        print(f'[Requirements matrix M] MSE = '
-              f'{mean_squared_error(self.exio_raw.satellite.M.values, my_M.values)}')
-
-        my_D_cba = self.exio_raw.satellite.M.copy()
-        my_D_cba = my_M * final_demand.values.T
-        #total values in the respective unit for the given characterization factor
-        print(f'[Consumption based accounting] MSE = '
-              f'{mean_squared_error(self.exio_raw.satellite.D_cba.values, my_D_cba.values)}')
+            self.S = self.S.iloc[:,strs_idx]
+            self.F = self.F.iloc[:,strs_idx]
+            self.Dcba = self.Dcba.iloc[:,line_idx]
--- test/huysman2016.py
+++ test/huysman2016.py
@@ -1,32 +1,102 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from framework import file_exiobase3
+
+def GWP_element_extractor(flow):
+        return flow[:3]
 
 EU27 = ['AT', 'BE', 'BG', 'CZ', 'CY', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GR', 'HU',
         'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'GB']
+EU_pop = 493000000
 
 year = 2007
 exio = file_exiobase3.EXIOfiles(EU27, year, region_filter=True)
-M, Y, A = exio.read()
+A, Dcba, F, M, S, Y, Z = exio.read()
 
-#basket-of-products
+#IO-model calculations (Leontief)
+house_occ = np.arange(0,len(Y.columns.values),7)
+Y = Y.iloc[:, house_occ]
+#out = Z @ np.ones((np.shape(Z)[1],1)) + Y @ np.ones((np.shape(Y)[1],1))
+f = Y @ np.ones((np.shape(Y)[1],1))
+#inv_out = np.diagflat(1/out)
+#inv_out[np.where(inv_out == np.inf)] = 0
+#A = Z @ inv_out
+L = np.linalg.inv(np.eye(len(A)) - A)
+output = L @ f
+output.rename(columns={0: "output"}, inplace=True)
+output.index = f.index
+reg = list()
+for idx in range(len(f.index)):
+        reg.append(f.index[idx][0])
+output['ISO_code'] = reg
+#output.rename(columns={0: "output"}, inplace=True)
+
+#filtering GWP stressors from the characterization vectors
+GWP_idx = list()
+stressor_GWP = ['CO2', 'N2O', 'CH4', 'SF6']
+stressor_idx = F.index.tolist()
+for i in range(len(stressor_idx)):
+        for j in stressor_GWP:
+                if stressor_idx[i].find(j) != -1:
+                        GWP_idx.append(i)
+                        continue
+
+total_stressor = F.iloc[GWP_idx,:]
+total_stressor.reset_index(inplace=True)
+total_stressor['stressor'] = total_stressor['stressor'].apply(GWP_element_extractor)
+
+#characterization factor (kg CO2 eq)
+GW_convert = {'CO2': 1, 'N2O': 1, 'CH4': 1, 'SF6': 1}
+conv_mat = np.zeros((len(total_stressor['stressor']),1))
+for stressor in range(np.shape(conv_mat)[0]):
+        conv_mat[stressor] = GW_convert[total_stressor['stressor'].values[stressor]]
+total_stressor.drop('stressor', axis=1, inplace=True)
+total_stressor = total_stressor * conv_mat
+
+#Environmental matrix
+x_hat = np.diagflat(1/output['output'])
+x_hat[np.where(x_hat == np.inf)] = 0
+B = total_stressor.iloc[:13,:] @ x_hat
+
+#Inventory vector
+g = B @ L
+g = g.sum(axis=0)
+footprint = g @ np.diagflat(f)
+footprint /= EU_pop
+footprint = pd.DataFrame(footprint)
+footprint.index = A.index
+
+#footprint
 path = "C:/Users/yamad/OneDrive/Documentos/FORCERA/GPP/GPP-data-inspection/data/conversions/"
 conv_HH = np.load(path+'conv_HH.npy')
 HH_atv = ['ISO_code','Food', 'Goods', 'Mobility', 'Shelter', 'Services']
-EU_basket = pd.DataFrame(columns = HH_atv)
-for reg in EU27:
-        curr_A = A[reg]
-        sum_A = curr_A.sum(axis=0).to_frame()
-        conv_A = sum_A.values * conv_HH
-        total_conv = conv_A.sum(axis=0)
-        entry = {'ISO_code': reg,'Food': total_conv[0],'Goods': total_conv[1], 'Mobility': total_conv[2],
+EU_footprint = pd.DataFrame(columns=HH_atv)
+footprint.index = A.index
+reg = list()
+for idx in range(len(footprint.index)):
+        reg.append(footprint.index[idx][0])
+footprint['ISO_code'] = reg
+footprint.rename(columns={0: "output"}, inplace=True)
+grouped_frame = footprint.groupby(footprint['ISO_code'])
+grp_keys = grouped_frame.groups.keys()
+for key in grp_keys:
+        curr_group = grouped_frame['output'].get_group(key)
+        conv_group = np.expand_dims(curr_group, axis=1) * conv_HH
+        total_conv = conv_group.sum(axis=0)
+        entry = {'ISO_code': key, 'Food': total_conv[0], 'Goods': total_conv[1], 'Mobility': total_conv[2],
                  'Shelter': total_conv[3], 'Services': total_conv[4]}
-        EU_basket.loc[len(EU_basket)] = entry
+        EU_footprint.loc[len(EU_footprint)] = entry
 
-#filtering GWP stressors from the characterization vectors
-test = M.index.tolist()
-
-#IO-model calculations
-f = Y @ np.ones((np.shape(Y)[1],1))
-output = np.linalg.inv(np.eye(len(A)) - A) @ f
+footprint_HH = EU_footprint.sum(axis=0)
+footprint_HH = footprint_HH.iloc[1:]
+footprint_HH /= 1000
+'''
+plt.figure()
+plt.bar(footprint_HH.index, footprint.values, width=0.4)
+plt.xlabel('Activity')
+plt.ylabel('tons CO2 eq per capita')
+plt.title('Global Warming Potential (GWP)')
+plt.grid()
+plt.show()
+'''
